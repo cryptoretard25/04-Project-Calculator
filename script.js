aa04b9a6cc439739bd26953666a18d8fc3972d17
cryptoretard25 <mikhmikhlobanov@gmail.com>
2022-09-08 16:20:08 +0300

    almost working

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const buttons = document.querySelectorAll(".btn");
 const container = document.querySelector(".btn-container");
 const input = document.querySelector("#main-input");
 const readOnly = document.querySelector("#storage-input");
-let calculate = [];
 
 class Button {
   constructor(element) {
@@ -26,30 +25,36 @@ class Button {
     const previous = calculator.previousValue;
     const operand = calculator.operand;
     //-----------------------------------------------------------------------------------------
+    //Early returns
     //handle with many zero at start
     if (current[0] === "0" && !current[1] && btn.value === "0") return;
     //handle with many dots
     if (btn.dataset.type === "decimal" && current.includes(".")) return;
+    //if 1st number is empty operand button unavaliable
+    if (!current && btn.dataset.type === "operand") return;
     //add zero before '.'
     if (btn.value === ".") calculator.currentValue = "0";
     //-----------------------------------------------------------------------------------------
     //if object properties filled and operand pressed calculate number
-    if (current && previous && operand && btn.dataset.type === "operand") {
+    //or calculate button pressed
+    if (
+      (current && previous && operand && btn.dataset.type === "operand") ||
+      btn.id === "calculate"
+    ) {
       calculator.currentValue = this.calculate(previous, current);
       calculator.previousValue = "";
       calculator.operand = btn.value;
     }
     //-----------------------------------------------------------------------------------------
-    
-    if (!current && btn.dataset.type === "operand") {
-      return;
-    } else if (btn.dataset.type !== "operand") {
+    //handle with first number
+    if (btn.dataset.type !== "operand") {
       input.value = btn.value;
       calculator.currentValue += btn.value;
       input.value = calculator.currentValue;
       readOnly.value += btn.value;
       log(calculator);
     }
+    //handle with operand and second number
     if (btn.dataset.type === "operand") {
       calculator.previousValue = calculator.currentValue;
       calculator.operand = btn.value;
@@ -58,50 +63,6 @@ class Button {
       calculator.currentValue = "";
       log(calculator);
     }
-    //-----------------------------------------------------------------------------------------
-    // if (calculate.length < 2) {
-    //   if (btn.dataset.type === "operand" && !input.value){ input.value = '0'; return};
-    //   if (btn.dataset.type === "number"|| btn.dataset.type ==='decimal') {
-    //     if(btn.dataset.type ==='decimal'&& input.value.includes('.')) return
-    //     input.value += btn.value;
-    //     readOnly.value += btn.value;
-    //     container.dataset.type = btn.dataset.type;
-    //   } else {
-    //     calculate[0] = input.value;
-    //     calculate[1] = btn.value;
-    //     input.value = "";
-    //     readOnly.value += btn.value;
-    //     container.dataset.type = btn.dataset.type;
-    //   }
-    // } else {
-    //   if (container.dataset.type === "operand" && btn.dataset.type === "operand") return;
-    //   if (btn.dataset.type === "number" || btn.dataset.type === "decimal") {
-    //     if (btn.dataset.type === "decimal" && input.value.includes(".")) return;
-    //     if (input.value == calculate[0]) {
-    //       input.value = "";
-    //     }
-    //     input.value += btn.value;
-    //     readOnly.value += btn.value;
-    //     container.dataset.type = btn.dataset.type;
-    //   } else if (
-    //     (container.dataset.type = "number" && btn.dataset.type === "operand")
-    //   ) {
-    //     calculate[2] = input.value;
-    //     input.value = "";
-    //     readOnly.value += btn.value;
-    //     this.calculate(calculate[0], calculate[2]);
-    //     calculate[1] = btn.value;
-    //     input.value = calculate[0];
-    //     container.dataset.type = "operand";
-    //   }else if(btn.id === 'calculate'){
-    //     calculate[2] = input.value;
-    //     input.value = '';
-    //     this.calculate(calculate[0], calculate[2]);
-    //     input.value = calculate[0];
-    //     readOnly.value = calculate[0];
-    //     container.dataset.type = "number";
-    //   }
-    // }
     //-----------------------------------------------------------------------------------------
   }
   ontransition(event) {
